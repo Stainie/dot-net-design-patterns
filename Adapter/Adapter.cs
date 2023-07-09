a1dba196d4e0cef0dd1930c1ffb1cc84d62cf57a
Stainie <stanko.kostic93@gmail.com>
2023-07-09 20:22:41 +0200

    added adapter

---
+++ Adapter/Adapter.cs
@@ -0,0 +1,33 @@
+﻿namespace Adapter
+{
+    public class Adaptee
+    {
+        public int Property;
+    }
+
+    public interface IAdapter
+    {
+        int PropertyA { get; }
+        int PropertyB { get; }
+    }
+
+    public static class ExtensionMethods
+    {
+        public static int PropSum(this IAdapter ia)
+        {
+            return ia.PropertyA + ia.PropertyB;
+        }
+    }
+
+    public class AdapteeToAdapter : IAdapter
+    {
+        public AdapteeToAdapter(Adaptee square)
+        {
+            PropertyA = square.Property;
+            PropertyB = square.Property;
+        }
+
+        public int PropertyA { get; }
+        public int PropertyB { get; }
+    }
+}
