ea9ee5cab93d4f5ebb44765cec5e9b0d6d681d0a
Stainie <stanko.kostic93@gmail.com>
2023-09-24 22:38:14 +0200

    added IRequest mediator MediatR library

---
+++ Mediator/RequestMediator.cs
@@ -0,0 +1,33 @@
+﻿using JetBrains.Annotations;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mediator
+{
+    public class RequestCommand : IRequest<ResponseCommand>
+    {
+    }
+
+    public class ResponseCommand
+    {
+        public string Response { get; set; }
+
+        public ResponseCommand(string response)
+        {
+            Response = response;
+        }
+    }
+
+    [UsedImplicitly]
+    public class RequestHandler : IRequestHandler<RequestCommand, ResponseCommand>
+    {
+        public async Task<ResponseCommand> Handle(RequestCommand request, CancellationToken cancellationToken)
+        {
+            return await Task.FromResult(new ResponseCommand("Response from RequestHandler")).ConfigureAwait(false);
+        }
+    }
+}
